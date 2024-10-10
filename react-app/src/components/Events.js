ff6fabc90b5b3af111d7ff71c834f4b1bdea7e39
Rosela Berberi <roselaberberi123@gmail.com>
2024-10-10 09:17:39 +0200

    test

--- react-app/src/components/Events.js
+++ react-app/src/components/Events.js
@@ -14,6 +14,7 @@ function EventsList()  {
         <div className="event-item">
           <h3>{event.title}</h3>
           <p>{event.date}</p>
+          
           <p>{event.description}</p>
           <button className="deleteEventBtn" data-id={event.id}>Delete</button>
         </div>
